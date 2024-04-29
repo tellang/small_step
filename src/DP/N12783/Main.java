7621a1560854821848b9ccdd0b643da95b4b0812
tellang <tellang@daum.net>
2024-04-29 19:22:36 +0900

    N12015 가장 긴 증가하는 부분 수열 3
    기존의 N11054 가장 긴 바이토닉 부분 수열 의 아이디어를 차용 후 자료구조를 Long 으로 변경하여 해결
    commit#f3ed71a1a42aa4ebf47cf9cfd5821e0ed9105e43

---
+++ src/DP/N12783/Main.java
@@ -0,0 +1,49 @@
+package DP.N12783;
+
+import static java.lang.Integer.max;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.util.Collections.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    final static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, MAX;
+    static List<Integer> dp = new ArrayList<>();
+    static List<Long> list = new ArrayList<>();
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        N = parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        list.add(parseLong(st.nextToken()));
+        dp.add(1);
+        MAX = 1;
+        for (int i = 1; i < N; i++) {
+            int num = parseInt(st.nextToken());
+            int orderSize = getOrderSize(num);
+            dp.add(orderSize);
+            MAX = max(MAX, orderSize);
+        }
+        System.out.println(MAX);
+    }
+
+    private static int getOrderSize(long num) {
+        if (list.get(list.size() - 1) < num){
+            list.add(num);
+            return list.size();
+        } else {
+            int idx = binarySearch(list, num);
+            if (idx < 0) idx = -idx - 1;
+
+            list.set(idx, num);
+            return idx+1;
+        }
+    }
+}
