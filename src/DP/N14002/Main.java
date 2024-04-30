be01935e79b245640eba3aa0eb7aab0a5a96f691
tellang <tellang@daum.net>
2024-04-30 14:23:49 +0900

    N14002 가장 긴 증가하는 부분 수열 4
    dp배열을 역탐색 하여 올바른 LIS 수열을 표기하도록 수정

---
+++ src/DP/N14002/Main.java
@@ -0,0 +1,63 @@
+package DP.N14002;
+
+import static java.lang.Integer.max;
+import static java.lang.Integer.parseInt;
+import static java.util.Collections.binarySearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    final static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, MAX;
+    static List<Integer> list = new ArrayList<>(),
+        originalLIS = new ArrayList<>(),
+        dp = new ArrayList<>();
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        N = parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        originalLIS.add(parseInt(st.nextToken()));
+        list.add(originalLIS.get(0));
+        dp.add(1);
+        MAX = 1;
+        for (int i = 1; i < N; i++) {
+            int num = parseInt(st.nextToken());
+            originalLIS.add(num);
+            int orderSize = getOrderSize(num);
+            dp.add(orderSize);
+            MAX = max(MAX, orderSize);
+        }
+        sb.append(MAX).append('\n');
+        int localMax = MAX - 1;
+        for (int oriIdx = dp.size() - 1; oriIdx >= 0 && localMax >= 0; oriIdx--) {
+            int dpIdx = dp.get(oriIdx) - 1;
+            if (dpIdx == localMax) {
+                sb.insert(2, originalLIS.get(oriIdx)).insert(2, ' ');
+                localMax--;
+            }
+        }
+        System.out.println(sb);
+    }
+
+    private static int getOrderSize(int num) {
+        if (list.get(list.size() - 1) < num) {
+            list.add(num);
+            return list.size();
+        } else {
+            int idx = binarySearch(list, num);
+            if (idx < 0)
+                idx = -idx - 1;
+
+            list.set(idx, num);
+            return idx + 1;
+        }
+    }
+}
