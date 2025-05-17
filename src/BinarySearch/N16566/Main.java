9275dad976d80231443459d19d6e916fc3753954
tellang <tellang@daum.net>
2025-05-17 12:53:10 +0900

    :white_check_mark: [BOJ] 16566번 - 카드 게임
    입력값을 정렬한 뒤, 이진 탐색으로 사용 가능한 카드 탐색.
    사용된 카드는 Union-Find 방식으로 연결하여 다음 사용 가능한 인덱스를 추적.
    카드 사용 후 인덱스 갱신을 통해 중복 선택 방지.

---
+++ src/BinarySearch/N16566/Main.java
@@ -0,0 +1,66 @@
+package BinarySearch.N16566;
+
+import static java.lang.Integer.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M, K;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static List<Integer> list = new ArrayList<>();
+	static List<Integer> indexList = new ArrayList<>();
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = parseInt(st.nextToken());
+		M = parseInt(st.nextToken());
+		K = parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		arr = new int[K];
+		for (int m = 0; m < M; m++) {
+			int e = parseInt(st.nextToken());
+			list.add(e);
+			indexList.add(m);
+		}
+		list.sort(Comparator.naturalOrder());
+		st = new StringTokenizer(br.readLine());
+
+		for (int k = 0; k < K; k++) {
+			arr[k] = parseInt(st.nextToken());
+		}
+
+		for (int i : arr) {
+			int result = Collections.binarySearch(list, i);
+			if (result < 0) {
+				result = (result+1)*(-1);
+			} else {
+				result++;
+			}
+			if (result >= M){
+				result = 0;
+			}
+			int rootIdx = findRootIdx(result);
+			if (rootIdx >= N) {
+				rootIdx = findRootIdx(0);
+			}
+			sb.append(list.get(rootIdx)).append('\n');
+			indexList.set(result, rootIdx+1);
+		}
+
+		System.out.println(sb);
+	}
+	public static int findRootIdx(int n){
+		if (indexList.get(n) == n)
+			return n;
+		else return findRootIdx(indexList.get(n));
+	}
+}
