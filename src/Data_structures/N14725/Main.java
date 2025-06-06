c4ec07dae6932d2321ebe6d0138b734a5ff034ab
tellang <tellang@daum.net>
2025-06-06 20:30:18 +0900

    :white_check_mark: [BOJ] 14725번 - 개미굴
    루트 노드에서 시작해 각 개미굴 경로를 트리 형태로 구성하고, TreeMap으로 자식 노드를 사전순 정렬.
    토큰 단위로 노드를 연결하며 중복 없이 삽입하고, DFS로 계층 깊이만큼 들여쓰기 출력.
    Node에 level 정보를 함께 저장해 출력 시 깊이 조절.
    
    Signed-off-by: tellang <tellang@daum.net>

---
+++ src/Data_structures/N14725/Main.java
@@ -0,0 +1,71 @@
+package Data_structures.N14725;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+/*
+이진트리 명시 없음
+애초에 트리가 맞긴한가?
+같은 층에 같은 이름이 없음
+ */
+public class Main {
+	static int N;
+	static Node root = new Node(null, "root");
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder result = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		N = Integer.parseInt(br.readLine());
+		for (int n = 0; n < N; n++) {
+			st = new StringTokenizer(br.readLine());
+			int K = Integer.parseInt(st.nextToken());
+			Node head = root;
+			Node child = null;
+			for (int k = 0; k < K; k++) {
+				String name = st.nextToken();
+				if (head.children.containsKey(name)) {
+					child = head.children.get(name);
+				} else
+					child = new Node(head, name);
+				head = child;
+			}
+		}
+
+		dfs(root);
+		System.out.println(result);
+	}
+
+	static void dfs(Node root) {
+		if (!root.children.isEmpty()) {
+			for (String childName : root.children.keySet()) {
+				result.append("--"
+						.repeat(Math.max(0, root.children.get(childName).level))
+					)
+					.append(childName)
+					.append('\n');
+				dfs(root.children.get(childName));
+			}
+		}
+	}
+}
+
+class Node {
+	Node root;
+	int level;
+	Map<String, Node> children = new TreeMap<>();
+
+	public Node(Node root, String name) {
+		this.root = root;
+		if (root != null) {
+			root.children.put(name, this);
+			this.level = root.level + 1;
+		} else {
+			this.level = -1;
+		}
+	}
+}
