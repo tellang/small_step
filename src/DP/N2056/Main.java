09beb7018fd037f39780b6e2c4d56e694f9a5f34
tellang <tellang@daum.net>
2025-06-16 14:24:13 +0900

    :white_check_mark: [BOJ] 2056번 - 작업
    위상 정렬 + DP 조합 방식으로 작업 순서와 누적 시간 계산.
    - 선행 작업이 모두 완료된 작업부터 처리되도록 우선순위 큐 활용
    - 각 작업마다 선행 작업의 최대 종료 시간을 더해 누적 소요 시간 갱신
    - 선행 작업 제거 시 후속 작업의 localMaxList 갱신으로 동적 계획 적용
    
    Signed-off-by: tellang <tellang@daum.net>

--- src/DP/N2056/Main.java
+++ src/DP/N2056/Main.java
@@ -1,9 +1,10 @@
 package DP.N2056;
 /*
-아무리 봐도 위상정렬인데
-DP도 써야하네
+선행 작업의 번호는 현재 작업보다 작다!
+10K x sigma(10k)
+= 10K x 10k(10k-1)/2
+=> 1000k/2 = 0.5M
  */
-
 
 import static java.lang.Integer.*;
 import static java.lang.System.*;
@@ -11,83 +12,34 @@ import static java.lang.System.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
 
-
-
 public class Main {
-	static Queue<Order> orders = new PriorityQueue<>(Comparator.comparingInt(Main::getPreSize));
-	static Set<Integer>[] preOrder;
-	static HashMap<Integer, Integer> time = new HashMap<>();
 	static int N;
 	static int K;
-	static int ORDER = 1;
-	static int RESULT = 0;
-	static int[] localMaxList;
+	static int MAX = 0;
+	static int[] timeList;
 	static BufferedReader br = new BufferedReader(new InputStreamReader(in));
 	static StringTokenizer st;
+
 	public static void main(String[] args) throws IOException {
 		N = parseInt(br.readLine());
-
-		preOrder = new HashSet[N+1];
-		for (int i = 0; i <= N; i++) {
-			preOrder[i] = new HashSet<>();
-		}
-		localMaxList = new int[N+1];
-		// orders.add(new Order(0, 0)); //dummy
-		for (int n = 0; n < N; n++) {
+		timeList = new int[N + 1];
+		for (int n = 1; n <= N; n++) {
 			st = new StringTokenizer(br.readLine());
-			HashSet<Integer> preset = new HashSet<>();
+
 			int t = parseInt(st.nextToken());
 			K = parseInt(st.nextToken());
-			for (int k = 0; k < K; k++) {
-				preset.add(parseInt(st.nextToken()));
-			}
-			time.put(ORDER, t);
-			orders.add(new Order(t, ORDER));
-			preOrder[ORDER] = preset;
-			ORDER++;
-
-		}
-
-
-		while (!orders.isEmpty()) {
-			Order poll = orders.poll();
+			timeList[n] = t;
 			int localMax = 0;
-			for (int p : preOrder[poll.n]) {
-				localMax = max(localMax, localMaxList[p]);
+			for (int k = 0; k < K; k++) {
+				localMax = Math.max(localMax, timeList[Integer.parseInt(st.nextToken())]);
 			}
-			for (int i = 0; i < preOrder.length; i++) {
-				Set<Integer> p = preOrder[i];
-				if (p.contains(poll.n)) {
-					p.remove(poll.n);
-					localMaxList[i] = Math.max(localMaxList[i], poll.t);
-				}
-			}
-
-			time.put(poll.n, time.get(poll.n) + localMax);
-			RESULT = Math.max(RESULT, time.get(poll.n));
+			timeList[n] += localMax;
+			MAX = Math.max(MAX, timeList[n]);
 		}
 
-		out.println(RESULT);
+		out.println(MAX);
 	}
-	static int getPreSize(Order order) {
-		return preOrder[order.n].size();
-	}
-}
-class Order {
-	int t, n;
 
-	public Order(int t, int n) {
-		this.t = t;
-		this.n = n;
-	}
 }
